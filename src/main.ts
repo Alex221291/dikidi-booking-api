cb585d367274508fe883ef356ae357853f2d2537
Alexandr Lukanev <lukanevteach@gmail.com>
2024-09-13 17:28:03 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -14,33 +14,9 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  console.log('LOCAL_HOST_NAME:', process.env.LOCAL_HOST_NAME);
-console.log('DEV_HOST_NAME:', process.env.DEV_HOST_NAME);
-
   app.enableCors({
     origin: [process.env.LOCAL_HOST_NAME, process.env.DEV_HOST_NAME],
     credentials: true,
-  });
-
-  app.use((req, res, next) => {
-    const allowedOrigins = [process.env.LOCAL_HOST_NAME, process.env.DEV_HOST_NAME];
-    const origin = req.headers.origin;
-    console.log(allowedOrigins);
-    console.log('========= ' + origin);
-    if (allowedOrigins.includes(origin)) {
-      console.log('=========true= ' + origin);
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-  
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
-  
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
-  
-    next();
   });
   
   app.useGlobalPipes(
